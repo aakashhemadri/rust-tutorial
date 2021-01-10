d5263ce86d841f2a6213329b65d5bec7a7b7a3bf
Aakash Hemadri <aakashhemadri123@gmail.com>
2021-01-10 21:36:05 +0530

    Add Section 4.3, Completing Chapter 4

---
+++ slicing/src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    println!("Hello, world!");
+    let mut s = String::from("Hello World");
+
+    let slice = first_word(&s);
+
+    // s.clear(); // error!
+
+    println!("the first word is: {}", slice);
+
+    // Other slices
+
+    let a = [1, 2, 3, 4, 5];
+
+    let _slice = &a[1..3];
+}
+
+fn first_word(s: &str) -> &str {
+    // Accept and return string slice
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+    &s[..]
+}
