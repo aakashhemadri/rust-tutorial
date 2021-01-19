10f36610ddb7efd53bbb590921287e342c352cf6
Aakash Hemadri <aakashhemadri123@gmail.com>
2021-01-19 19:28:13 +0530

    Chapter 8

---
+++ chapter_8/vector/src/main.rs
@@ -0,0 +1,38 @@
+fn main() {
+    let mut vx: Vec<i32> = Vec::new(); // 
+    let mut vy = vec![1,2,3]; // Using Macros for default i32
+    vx.push(5);
+    vy.push(6);
+    let v = vec![1, 2, 3, 4, 5];
+    let third: &i32 = &v[2];
+    println!("The third element is {}", third);
+
+    match v.get(2) {
+        Some(third) => println!("The third element is {}", third),
+        None => println!("There is no third element."),
+    }
+
+    for i in &v {
+        println!("{}", i);
+    }
+
+    let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50;
+    }
+    for i in &v {
+        println!("{}", i);
+    }
+
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let _row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+}
